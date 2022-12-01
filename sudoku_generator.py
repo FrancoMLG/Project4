a80a4114cf634fcfffa967d286c5c3b2311fbbb7
hutch <hutchinson.frank@ufl.edu>
2022-11-30 21:08:06 -0500

    sudoku main

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,21 +1,8 @@
-from pygame.locals import *
+import pygame
+pygame.init()
+WINDOW = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('Sudoku')
 
-WIDTH = 600
-HEIGHT = 600
-LINE_WIDTH = 15
-WIN_LINE_WIDTH = 15
-BOARD_ROWS = 3
-BOARD_COLS = 3
-SQUARE_SIZE = 200
-CIRCLE_RADIUS = 60
-CIRCLE_WIDTH = 15
-CROSS_WIDTH = 25
-SPACE = 55
-RED = (255, 0, 0)
-BG_COLOR = (255, 255, 245)
-LINE_COLOR = (245, 152, 66)
-CIRCLE_COLOR = (155, 155, 155)
-CROSS_COLOR = (66, 66, 66)
 def generate_sudoku(size, removed):
     pass
 
@@ -26,7 +13,6 @@ class SudokuGenerator:
     def get_board(self):
         board = [["-" for i in range(self.row_length)] for j in range(self.row_length)]
         return board
-
     def print_board(self):
         board = self.get_board
         for row in board:
@@ -109,5 +95,4 @@ class Board:
         # this checks for that varibles
         # if player wins = game_continue False
         # if player loses = game_continue False but player is able to restart
-
         pass
