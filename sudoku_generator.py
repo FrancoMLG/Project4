fc5f20cc8a9fc4c1fb75529e758e110fa727e7de
hutch <hutchinson.frank@ufl.edu>
2022-12-03 22:11:48 -0500

    sudoku main

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 WIDTH = 500
 HEIGHT = 500
@@ -12,65 +13,139 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 pygame.quit()
+def __init__(self, removed_cells, row_length=9):
+    self.row_length = row_length
+    self.removed_cells = removed_cells
+    self.board = Board(WIDTH, HEIGHT, WINDOW, self.removed_cells)
+def get_board(self):
+    return self.board.get_board_values()
+    # board = [["-" for i in range(self.row_length)] for j in range(self.row_length)]
+    # return board
+def print_board(self):
+    board = self.get_board()
+    for i in range(0, 9):
+        row = board[i]
+        print(row)
+        print()
+    # board = self.get_board
+    # for row in board:
+    # for col in row:
+    # print(col, end= '')
+    # print()
+def valid_in_row(self, row, num):
+    if not isinstance(row, int):  # row is int
+        return False
+    if not isinstance(num, int):  # num is int
+        return False
+    if num < 1 or num > 9:
+        return False
+    if row >= len(self.board) or row < 0:
+        return False
+    for col in range(len(self.board[row])):
+        if self.board[row][col] == num:
+            return False
+    return True
+def valid_in_col(self, col, num):
+    if not isinstance(col, int):  # row is int
+        return False
+    if not isinstance(num, int):  # num is int
+        return False
+    if num < 1 or num > 9:
+        return False
+    if col >= len(self.board[0]) or col < 0:
+        return False
+    for row in range(len(self.board)):
+        if self.board[row][col] == num:
+            return False
+    return True
+def valid_in_box(self, row_start, col_start, num):
+    if not isinstance(row_start, int):
+        return False
+    if not isinstance(col_start, int):
+        return False
+    if not isinstance(num, int):
+        return False
+    if row_start % 3 != 0 or col_start % 3 != 0:
+        return False
+    if num < 1 or num > 9:
+        return False
+    for row in range(row_start, row_start + 3):
+        for col in range(col_start, col_start + 3):
+            if self.board[row][col] == num:
+                return False
+    return True
+def is_valid(self, row, col, num):
+    if not isinstance(row, int):
+        return False
+    if not isinstance(col, int):
+        return False
+    if not isinstance(num, int):
+        return False
+    return self.valid_in_row(row, num) and \
+           self.valid_in_col(col, num) and \
+           self.valid_in_box(row - row % 3, col - col % 3, num)
+def fill_box(self, row_start, col_start):
+    if not isinstance(row_start, int):
+        return False
+    if not isinstance(col_start, int):
+        return False
+    row_start -= row_start % 3
+    col_start -= col_start % 3
+    for row in range(row_start, row_start + 3):
+        for col in range(col_start, col_start + 3):
+            if self.board[row][col] != 0:
+                continue
+            while True:
+                num = random.randint(1, 9)
+                if self.is_valid(row, col, num):
+                    break
+            self.board[row][col] = num
+def fill_diagonal(self):
+    for pos in range(0, 9, 3):
+        self.fill_box(pos, pos)
+def fill_remaining(self, row, col):
+    if (col >= self.row_length and row < self.row_length - 1):
+        row += 1
+        col = 0
+    if row >= self.row_length and col >= self.row_length:
+        return True
+    if row < self.box_length:
+        if col < self.box_length:
+            col = self.box_length
+    elif row < self.row_length - self.box_length:
+        if col == int(row // self.box_length * self.box_length):
+            col += self.box_length
+    else:
+        if col == self.row_length - self.box_length:
+            row += 1
+            col = 0
+            if row >= self.row_length:
+                return True
 
-def generate_sudoku(size, removed):
-    pass
-
-class SudokuGenerator:
-    def __init__(self, removed_cells, row_length = 9):
-        self.row_length = row_length
-        self.removed_cells = removed_cells
-        self.board = Board(WIDTH, HEIGHT, WINDOW, self.removed_cells)
-    def get_board(self):
-        return self.board.get_board_values()
-        #board = [["-" for i in range(self.row_length)] for j in range(self.row_length)]
-        #return board
-    def print_board(self):
-        board = self.get_board()
-        for i in range(0, 9):
-            row = board[i]
-            print(row)
-            print()
-        #board = self.get_board
-        #for row in board:
-            #for col in row:
-                #print(col, end= '')
-            #print()
-    def valid_in_row(self, row, num):
-        self.row = row
-        self.num = num
-        board = self.get_board()
-        checking_row = board[row]
-        return num not in checking_row
-    def valid_in_col(self, col, num):
-        self.col = col
-        self.num = num
-        board = self.get_board()
-        checking_col = board[col]
-        return num not in checking_col
-    def valid_in_box(self, row_start, col_start, num):
-        self.row_start = row_start
-        self.col_start = col_start
-        self.num = num
-        return
-    def is_valid(self, row, col, num):
-        self.row = row
-        self.col = col
-        self.num = num
-    def fill_box(self, row_start, col_start):
-        self.row_start = row_start
-        self.col_start = col_start
-        pass
-    def fill_diagonal(self):
-        pass
-    def fill_remaining(self, row, col):
-        self.row = row
-        self.col = col
-        pass
-    def fill_values(self):
-        pass
-    def remove_cells(self):
-        pass
+    for num in range(1, self.row_length + 1):
+        if self.is_valid(row, col, num):
+            self.board[row][col] = num
+            if self.fill_remaining(row, col + 1):
+                return True
+            self.board[row][col] = 0
+    return False
+def fill_values(self):
+    self.fill_diagonal()
+    self.fill_remaining(0, self.box_length)
+def remove_cells(self):
+    removed_cells = []
+    index = 1
+    while True:
+        if index == self.removed_cells:
+            break
+        row = random.randint(0, 8)
+        col = random.randint(0, 8)
+        if[row, col] in removed_cells:
+            continue
+        else:
+            self.board[row][col] = 0
+            removed_cells.append([row, col])
+            index += 1
 
 class Cell:
     def __init__(self, value, row, col, screen):
