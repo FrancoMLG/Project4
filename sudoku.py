a5c82241d0e4f58b00b0ac975ad34722bc3c59a0
hutch <hutchinson.frank@ufl.edu>
2022-12-05 18:25:03 -0500

    sudoku main

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,21 @@
 from sudoku_generator import SudokuGenerator, Cell, Board
+import pygame
+
+pygame.init()
+WIDTH = 500
+HEIGHT = 500
+dif = WIDTH / 9
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Sudoku')
+run = True
+background_img = pygame.image.load('sudoku.jpg')
+background_img = pygame.transform.scale(background_img, (500, 500))
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
 
 if __name__ == '__main__':
     pass
--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,20 +1,6 @@
 import pygame
 import random
-pygame.init()
-WIDTH = 500
-HEIGHT = 500
-dif = WIDTH / 9
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Sudoku')
-run = True
-background_img = pygame.image.load('sudoku.jpg')
-background_img = pygame.transform.scale(background_img, (500, 500))
-while run:
-    pygame.time.delay(1000)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-pygame.quit()
+
 def generate_sudoku(size, removed):
     pass
     sudoku = SudokuGenerator(size, removed)
