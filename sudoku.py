740b8d60648d2210cded0097da8f76ec7ab5fc0a
Kevin Concha <39099548+kconcha02@users.noreply.github.com>
2022-12-05 21:33:55 -0500

    added opening screen, needs button functionality

--- sudoku.py
+++ sudoku.py
@@ -1,23 +1,6 @@
-from sudoku_generator import SudokuGenerator, Cell, Board
-import pygame
+import pygame.draw
 
-pygame.init()
-WIDTH = 500
-HEIGHT = 500
-dif = WIDTH / 9
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Sudoku')
-running = True
-background_img = pygame.image.load('sudoku.jpg')
-background_img = pygame.transform.scale(background_img, (500, 500))
-while running:
-    WINDOW.fill((0, 0, 0))
-    WINDOW.blit(background_img,(0,0))
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-#pygame.quit()
+from sudoku_generator import SudokuGenerator, Cell, Board
 
 def main():
     pass
